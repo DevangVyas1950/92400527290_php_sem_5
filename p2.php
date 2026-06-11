bab8fc19eab84a28599e63d0cb0c074940de77cf
DevangVyas1950 <devuvyas1950@gmail.com>
2026-06-11 12:24:28 +0530

    Add files via upload

---
+++ p2.php
@@ -0,0 +1,13 @@
+<?php
+$str = "Hello World";
+echo $str  ."devang";
+print("<br>");
+print($str. "devang");
+
+$numm = "123";
+echo "<br>";    
+var_dump($numm);
+
+// echo "<br>";
+// $abc = var_export($numm, true);
+// echo $abc;
---
+++ p3.php
@@ -0,0 +1,10 @@
+<?php
+   function mytest()
+   {
+    $lang ="PHP";
+    echo "Web development language: ". $lang;
+    }
+    mytest();
+    echo $lang;
+    ?>
+   
