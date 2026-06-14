f7ee7345891f526025a817d34c09109ae75f91ea
DevangVyas1950 <devuvyas1950@gmail.com>
2026-06-14 10:28:12 +0530

    Add files via upload

---
+++ p4.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = "Devang vyas"; //Global Variable
+
+function global_var()
+
+{
+
+global $name;
+
+echo "Variable inside the function: ". $name;
+
+echo "</br>";
+
+}
+
+global_var();
+
+echo "Variable outside the function: ". $name;
+
+?>
